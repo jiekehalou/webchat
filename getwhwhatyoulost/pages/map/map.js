f5eb6c3d7fd2aad45691839e9362e6879f966c31
王浩杰 <wanghaojie@wintelia.com>
2026-03-18 16:04:46 +0800

    00

--- getwhwhatyoulost/pages/map/map.js
+++ getwhwhatyoulost/pages/map/map.js
@@ -1,105 +1,180 @@
 // pages/map/map.js
-import {
-  KEY
-} from "../../config/index.js"
-const bg = 'https://pic.616pic.com/bg_w1180/00/00/02/zGDDPXuN2S.jpg'
+import { KEY } from "../../config/index.js"
 
-const headImg = 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2Fd064be90-6b8c-4a6d-9721-837206fbb4a7%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1696759051&t=b90e0bf861e41c30214fc739ae66343f'
-const {
-  windowHeight
-} = wx.getSystemInfoSync()
+const { windowHeight } = wx.getSystemInfoSync()
 const menuRect = wx.getMenuButtonBoundingClientRect()
 const sheetHeight = (windowHeight - menuRect.height) * 0.5
 console.log('KEY', KEY)
 Page({
 
-      /**
-       * 页面的初始数据
-       */
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    latitude: 39.9,
+    longitude: 116.38,
+    menuRect,
+    sheetHeight,
+    minSize: 0.3,
+    maxSize: 0.5,
+    weatherInfo: {
+
+    },
+    markers: [
+
+    ], polylines: [
+      {
+        points: [{
+          longitude: 113.67739,
+          latitude: 34.75381,
+        }, {
+          longitude: 112.67739,
+          latitude: 34.75381,
+        }],
+        color: '#58c16c',
+        width: 6,
+        borderColor: '#2f693c',
+        borderWidth: 1
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  async onLoad(options) {
+    await this.getPosition()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    // this.createSelectorQuery()
+    // .select(".sheet")
+    // .node()
+    // .exec(res => {
+    //   const sheetContext = res[0].node
+    //   sheetContext.scrollTo({
+    //     size: 0.7,
+    //     animated: true,
+    //     duration: 300,
+    //     easingFunction: 'ease'
+    //   })
+    // })
+  },
+
+  getWeather() {
+    let self = this;
+    wx.request({
+      url: 'https://apis.map.qq.com/ws/weather/v1/',
+      method: 'GET',
       data: {
-        latitude: 39.9,
-        longitude: 116.38,
-        menuRect,
-        sheetHeight,
-        minSize: 0.3,
-        maxSize: 0.5,
-        headImg,
-        weatherInfo: {
-
-        }
+        key: KEY,
+        location: `${this.data.latitude},${this.data.longitude}`,
       },
-
-      /**
-       * 生命周期函数--监听页面加载
-       */
-      async onLoad(options) {
-        await this.getPosition()
+      header: {
+        'content-type': 'application/json' // 默认值
       },
-
-      /**
-       * 生命周期函数--监听页面初次渲染完成
-       */
-      onReady() {
-        // this.createSelectorQuery()
-        // .select(".sheet")
-        // .node()
-        // .exec(res => {
-        //   const sheetContext = res[0].node
-        //   sheetContext.scrollTo({
-        //     size: 0.7,
-        //     animated: true,
-        //     duration: 300,
-        //     easingFunction: 'ease'
-        //   })
-        // })
-      },
-
-      getWeather() {
-        let self = this;
-        wx.request({
-          url: 'https://apis.map.qq.com/ws/weather/v1/',
-          method: 'GET',
-          data: {
-            key: KEY,
-            location: `${this.data.latitude},${this.data.longitude}`,
-          },
-          header: {
-            'content-type': 'application/json' // 默认值
-          },
-          success(res) {
-            let {
-              result
-            } = res.data
-            let {
-              realtime = []
-            } = result
-            let d = (realtime || [])[0] || {}
-            self.setData({
-              weatherInfo: d
-            })
-          }
+      success(res) {
+        let {
+          result
+        } = res.data
+        let {
+          realtime = []
+        } = result
+        let d = (realtime || [])[0] || {}
+        self.setData({
+          weatherInfo: d
+        })
+      }
+    })
+  },
+  addMark(res) {
+    let { longitude: lon, latitude: lat } = res
+    console.log('lon', lon)
+    console.log('lat', lat)
+   
+    let markers = [{
+      id: Date.now(),
+      latitude: lat,
+      longitude: lon,
+      iconPath: "/images/pin.png",
+      width: 48,
+      height: 48,
+      callout: {
+        content: `当前位置`,
+        display: 'ALWAYS',
+        color: '#333',
+        fontSize: 14,
+        borderWidth: 2,
+        borderRadius: 10,
+        bgColor: '#fff',
+        padding: 5,
+      }
+    },
+    {
+      id: Date.now(),
+      latitude: 33.62,
+      longitude: 113.37,
+      iconPath: "/images/pin.png",
+      width: 48,
+      height: 48,
+      callout: {
+        content: `目的地`,
+        display: 'ALWAYS',
+        color: '#333',
+        fontSize: 14,
+        borderWidth: 2,
+        borderRadius: 10,
+        bgColor: '#fff',
+        padding: 5,
+      }
+    }
+    ]
+    console.log('markers', markers)
+    this.setData({
+      markers: markers
+    }, () => {
+      console.log(this.data)
+    })
+  },
+  onMapClick(e){
+    const { latitude, longitude } = e.detail;
+    // his.reverseGeocode(latitude, longitude); // 调用逆地理编码
+    console.log( longitude,latitude,)
+    console.log('onMapClick',e)
+  },
+  reverseGeocode(lat, lng) {
+    wx.request({
+      url: `https://apis.map.qq.com/ws/geocoder/v1/?location=${lat},${lng}&key=您的KEY`,
+      success: (res) => {
+        console.log('详细地址:', res.data.result.address);
+      }
+    });
+  },
+  
+  async getPosition() {
+    wx.getLocation({
+      type: 'wgs84',
+      success: (res) => {
+        var latitude = res.latitude // 纬度
+        var longitude = res.longitude // 经度
+        // console.log("positionInfo",res)
+        this.setData({
+          longitude,
+          latitude
+        }, () => {
+          this.addMark(res)
+          this.getWeather()
         })
       },
-      async getPosition() {
-        wx.getLocation({
-            type: 'wgs84',
-            success: (res) => {
-              var latitude = res.latitude // 纬度
-              var longitude = res.longitude // 经度
-              // console.log("positionInfo",res)
-              this.setData({
-                    longitude,
-                    latitude
-                  }, () => {
-                    this.getWeather()
-                  })
-                },
-                fail: () => {
-                  this.setData({
-                    longitude: null,
-                    latitude: null,
-                  })
-                }
-            })
-        }
-      })
+      fail: () => {
+        this.setData({
+          longitude: null,
+          latitude: null,
+        })
+      }
+    })
+  }
+})
