1fbbad779797467d1e99fda4368cdef13002e1fb
王浩杰 <wanghaojie@wintelia.com>
2026-03-18 17:21:21 +0800

    "libVersion": "3.14.3"

--- getwhwhatyoulost/app.js
+++ getwhwhatyoulost/app.js
@@ -9,18 +9,29 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log('login-res:', res?.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    wx.getUserInfo({
+      success: res => {
+        console.log('getUserInfo-res:', res.userInfo)
+        // 可以将 res 发送给后台解码出 unionId
+        this.globalData.userInfo = res.userInfo
+        console.log(res.userInfo)
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log('getSetting-res:', res.userInfo)
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              // this.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
