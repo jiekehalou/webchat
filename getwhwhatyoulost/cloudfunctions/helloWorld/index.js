b240b13cbdef56d0cfb21502a2587739fa7b26e3
王浩杰 <wanghaojie@wintelia.com>
2026-03-25 16:30:27 +0800

    云服务 增删改查 ok

--- getwhwhatyoulost/cloudfunctions/helloWorld/index.js
+++ getwhwhatyoulost/cloudfunctions/helloWorld/index.js
@@ -1,16 +1,90 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+}) // 使用当前云环境
+// 获取数据库引用
+const db = cloud.database({
+  env: 'cloud1-0gf20vi35a83c542'
+})
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  console.log('收到的参数：', event)
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+  // 查询数据
+  let response = {}
+  let {
+    TYPE,
+    id,
+    ...other
+  } = event || {};
+  switch (TYPE) {
+    case 'add':
+      // 新增数据
+      response = await db.collection('logs').add({
+        data: {
+          name: event.name,
+          age: event.age,
+          createTime: db.serverDate() // 服务器时间
+        },
+        success: function (res) {
+          // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+        }
+      })
+
+
+      break;
+    case 'update':
+      // 更新数据
+      try {
+        // 使用 await，结果通过返回值获取
+        const response = await db.collection('logs')
+          .doc(id)
+          .update({
+            data: {
+              ...other
+            }
+          })
+        // response 结构：{ stats: { updated: 1 } }
+        if (response.stats.updated > 0) {
+          // console.log('更新成功')
+        }
+      } catch (err) {
+        // console.error('更新失败', err)
+      }
+      break
+    case 'delete':
+      // 更新数据
+      try {
+        // 使用 await，结果通过返回值获取
+        const response = await db.collection('logs')
+          .doc(id)
+          .remove()
+        if (response.stats.updated > 0) {
+          // console.log('删除成功')
+        }
+      } catch (err) {
+        console.error('删除失败', err)
+      }
+      break
+    default:
+      response = await db.collection('logs')
+        .get()
+      break;
   }
+
+
+
+
+
+
+  return response
+
+  // const wxContext = cloud.getWXContext()
+  // return {
+  //   event,
+  //   openid: wxContext.OPENID,
+  //   appid: wxContext.APPID,
+  //   unionid: wxContext.UNIONID,
+  // }
 }
--- getwhwhatyoulost/pages/logs/logs.js
+++ getwhwhatyoulost/pages/logs/logs.js
@@ -5,33 +5,100 @@ Page({
   data: {
     logs: []
   },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-
+  cloudInit() {
     // 页面初始化时，确保云环境已初始化（在 app.js 中）
-// 如果还没初始化，在 app.js 的 onLaunch 中添加：
-wx.cloud.init({
-  env: 'cloud1-0gf20vi35a83c542'  // 在云开发控制台可查看
-})
+    // 如果还没初始化，在 app.js 的 onLaunch 中添加：
+    wx.cloud.init({
+      env: 'cloud1-0gf20vi35a83c542' // 在云开发控制台可查看
+    })
+  },
+  handelDel() {
+    // 调用云函数
+    let id = this.data.logs[0]?._id
+    console.log('handelDel', id)
+    wx.cloud.callFunction({
+      name: 'helloWorld', // 云函数名称
+      data: { // 传递给云函数的参数
+        id,
+        TYPE: "delete", //add update delete 默认''查询
+      },
+      success: (res) => {
+        this.handleQuery()
+      },
+      fail: (err) => {
+        console.error('调用失败：', err)
+      }
+    })
+  },
+  handleUpdate() {
+    // 调用云函数
+    let id = this.data.logs[0]?._id
+    console.log('更新', id)
+    wx.cloud.callFunction({
+      name: 'helloWorld', // 云函数名称
+      data: { // 传递给云函数的参数
+        id,
+        name: '测试更新',
+        age: Math.floor(Math.random() * 100),
+        TYPE: "update", //add update delete 默认''查询
+      },
+      success: (res) => {
+        this.handleQuery()
+      },
+      fail: (err) => {
+        console.error('调用失败：', err)
+      }
+    })
+  },
+  handleQuery() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'helloWorld', // 云函数名称
+      data: { // 传递给云函数的参数
+        TYPE: "", //add update delete 默认''查询
+      },
+      success: (res) => {
+        // res.result 就是云函数 return 的数据
+        let arr = res.result?.data || []
+        this.setData({
+          logs: arr
+        })
+      },
+      fail: (err) => {
+        console.error('调用失败：', err)
+      }
+    })
+  },
+  handleAdd() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'helloWorld', // 云函数名称
+      data: { // 传递给云函数的参数
+        name: '张三',
+        age: 25,
+        TYPE: "add", //add update delete
+      },
+      success: (res) => {
+        // console.log('云函数返回结果：', res.result)
+        this.handleQuery()
+        // res.result 就是云函数 return 的数据
+      },
+      fail: (err) => {
+        console.error('调用失败：', err)
+      }
+    })
+  },
+  onLoad: function () {
+    // this.setData({
+    //   logs: (wx.getStorageSync('logs') || []).map(log => {
+    //     return {name:util.formatTime(new Date(log))}
+    //   })
+    // })
+    this.cloudInit()
+    this.handleQuery()
 
-// 调用云函数
-wx.cloud.callFunction({
-  name: 'helloWorld',  // 云函数名称
-  data: {              // 传递给云函数的参数
-    name: '张三',
-    age: 25
-  },
-  success: (res) => {
-    console.log('云函数返回结果：', res.result)
-    // res.result 就是云函数 return 的数据
-  },
-  fail: (err) => {
-    console.error('调用失败：', err)
-  }
-})
+
+
+
   }
 })
